b82769ba0c2ac6e80c29b0f462bde06d1357bb64
mdesevricourt <m.desevricourt@hotmail.fr>
2023-09-03 15:25:08 -0400

    Update test.py
    Trying to write some tests for agent and market classes

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 import unittest 
+
+from market import Market
+from cubicle import Cubicle
+from agent import Agent
+
+import numpy as np
+
+class TestAgent(unittest.TestCase):
+    def setUp(self) -> None:
+        U = np.zeros((2, 2))
+        np.fill_diagonal(U, [1, 1])
+        agent = Agent("Alice", np.array([[0, 1], [0, 0]]), 100)
+        return 
+class TestMarket(unittest.TestCase):
+
+
