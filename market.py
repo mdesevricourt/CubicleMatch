05a9946c7dea64f836330df2e947c17f96debaa3
mdesevricourt <m.desevricourt@hotmail.fr>
2023-09-03 21:03:48 -0400

    Update market.py
    Added aggregate demand

--- market.py
+++ market.py
@@ -11,11 +11,11 @@ class Market:
     def __init__(self, agents, cublicles) -> None:
         self.agents = agents
         self.cublicles = cublicles
+        self.cublicles_names = [cubicle.number for cubicle in cublicles]
         self.numberofagents = len(agents)
         self.numberofcublicles = len(cublicles)
         self.numberofhalfdays = cublicles[0].numberofhalfdays
         self.bundles = [np.array(t) for t in itertools.product([0, 1], repeat=self.numberofhalfdays)]
-        
 
     @property
     def prices_array(self):
@@ -23,7 +23,7 @@ class Market:
         return np.array([cubicle.prices for cubicle in self.cublicles])
     
     @prices_array.setter
-    def update_prices_array(self, prices):
+    def prices_array(self, prices):
         """Set the prices of the cublicles"""
         # check that prices is the right length
         assert len(prices) == self.numberofcublicles
@@ -36,13 +36,13 @@ class Market:
         return np.array([cubicle.prices for cubicle in self.cublicles]).flatten()
     
     @prices_vec.setter
-    def update_prices_vec(self, prices):
+    def prices_vec(self, prices):
         """Set the prices of the cublicles"""
         
         # divide the prices into the cublicles
         prices = np.array(prices).reshape(self.numberofcublicles, self.numberofhalfdays)
         # use the update prices_array method
-        self.update_prices_array(prices)
+        self.prices_array = prices
 
     def price_bundle(self, bundle):
         """Return the lowest price of a bundle of half-days across all cublicles, as wee as the name of the cublicle that has the lowest price"""
@@ -76,6 +76,23 @@ class Market:
         princed_bundles = sorted(princed_bundles, key=lambda x: x[2])
 
         return princed_bundles
+    
+    def aggregate_demand(self):
+        """Return the aggregate demand of all the agents, given the current prices of the cublicles"""
+        # for each cublicle, create a list representing the demand for that cublicle
+        demand_for_cublicles = [0] * self.numberofcublicles * self.numberofhalfdays
+        
+        
+        # for each agent, find the bundle that maximizes the utility of the agent
+        for agent in self.agents:
+            bundle, cubicle, _ = agent.find_agent_demand(self.priced_bundles)
+            # if the agent is assigned to a bundle
+            if bundle is not None:
+                # update the demand for that cublicle
+                demand_for_cublicles[self.cublicles_names.index(cubicle) * self.numberofhalfdays: self.cublicles_names.index(cubicle) * self.numberofhalfdays + self.numberofhalfdays] += bundle
+        
+        
+        return demand_for_cublicles
 
     
 
