4fee1873d2c3332f155cb0d357dfd7b1a786fbe2
mdesevricourt <m.desevricourt@hotmail.fr>
2024-01-01 16:24:20 -0500

    add tests

--- cubiclematch_jax/tests/test_demand.py
+++ cubiclematch_jax/tests/test_demand.py
@@ -1,7 +1,14 @@
 import jax
 import jax.numpy as jnp
+import pytest
 
-from cubiclematch_jax.demand import aggregate_demand, demand_vector, individual_demand
+from cubiclematch_jax.demand import (
+    compute_aggregate_demand,
+    demand_vector,
+    find_agent_demand,
+    individual_demand,
+)
+from cubiclematch_jax.market_level import aggregate_quantities
 from cubiclematch_jax.preferences import create_U_tilde
 from cubiclematch_jax.price import price_bundles
 
@@ -29,7 +36,7 @@ def test_individual_demand():
     bundle_prices = price_bundles(bundles, prices)
     U_tilde = create_U_tilde(U, u_cubicle)
     expected = bundle2
-    demanded_bundle, _ = individual_demand(budget, U, u_cubicle, bundles, bundle_prices)
+    demanded_bundle, _ = individual_demand(budget, U_tilde, bundles, bundle_prices)
     assert jnp.allclose(demanded_bundle, expected)
 
 
@@ -76,7 +83,7 @@ def test_vector_demand():
     expected2 = bundle2
     expected = jnp.array([expected1, expected2])
 
-    demanded_bundles, _ = demand_vector(budgets, U, u_cubicle, bundles, bundle_prices)
+    demanded_bundles, _ = demand_vector(budgets, U_tilde, bundles, bundle_prices)
     print(demanded_bundles)
     assert jnp.allclose(demanded_bundles, expected)
 
@@ -115,19 +122,55 @@ def test_aggregate_demand(verbose=False):
     bundles = jnp.array([bundle1, bundle2, bundle3, bundle4, bundle5])
 
     prices = jnp.array([10, 10, 10, 10, 10, 5, 5, 5, 5, 5])
-    bundle_prices = price_bundles(bundles, prices)
 
     bundles = jnp.array([bundle1, bundle2, bundle3, bundle4, bundle5])
 
     expected1 = bundle1
     expected2 = bundle2
-    agg_demand, _ = aggregate_demand(budgets, U, u_cubicle, bundles, bundle_prices)
+    res = aggregate_quantities(
+        prices=prices,
+        budgets=budgets,
+        U_tilde=U_tilde,
+        bundles=bundles,
+        supply=jnp.ones(
+            [
+                10,
+            ]
+        ),
+    )
+    agg_demand = res["agg_demand"]
     if verbose:
         print(agg_demand)
     assert jnp.allclose(agg_demand, expected1 + expected2)
+
+
+# Test cases
+def test_find_agent_demand_all_demand():
+    demanded_bundles = jnp.array([[1, 1], [1, 1]])
+    index = 0
+    assert jnp.array_equal(
+        find_agent_demand(index, demanded_bundles), jnp.array([0, 1])
+    )
+
+
+def test_find_agent_demand_no_demand():
+    demanded_bundles = jnp.array([[0, 0], [0, 0]])
+    index = 0
+    assert jnp.array_equal(find_agent_demand(index, demanded_bundles), jnp.array([]))
+
+
+def test_find_agent_demand_mixed_demand():
+    demanded_bundles = jnp.array([[1, 0], [0, 1], [1, 1]])
+    index = 1
+    assert jnp.array_equal(
+        find_agent_demand(index, demanded_bundles), jnp.array([1, 2])
+    )
 
 
 if __name__ == "__main__":
     test_individual_demand()
     test_vector_demand()
     test_aggregate_demand(verbose=True)
+    test_find_agent_demand_all_demand()
+    test_find_agent_demand_no_demand()
+    test_find_agent_demand_mixed_demand()
---
+++ cubiclematch_jax/tests/test_find_ACE.py
@@ -0,0 +1,48 @@
+import jax
+import jax.numpy as jnp
+
+from cubiclematch_jax.find_ACE import (
+    filter_out_tabu_neighbors,
+    find_neighbor_with_smallest_error,
+    generate_random_price_vector,
+)
+
+
+def test_generate_random_price_vector():
+    """Test the generate_random_price_vector function"""
+    key = jax.random.PRNGKey(0)
+    length = 10
+    total_budget = 100
+    price_vector, key = generate_random_price_vector(length, total_budget, key)
+
+    assert price_vector.shape == (length,)
+    assert jnp.all(price_vector >= 0)
+    assert jnp.all(price_vector <= total_budget)
+
+    price_vector_2, key = generate_random_price_vector(length, total_budget, key)
+
+    assert not jnp.allclose(price_vector, price_vector_2)
+
+
+def test_filter_out_tabu_neighbors():
+    """Test the filter_out_tabu_neighbors function"""
+    neighbors = jnp.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [8, 9, 10]])
+    tabu_list = jnp.array([[1, 2, 3], [7, 8, 9]])
+    non_tabu_neighbors = filter_out_tabu_neighbors(neighbors, tabu_list)
+
+    assert jnp.allclose(non_tabu_neighbors, jnp.array([[4, 5, 6], [8, 9, 10]]))
+
+
+def test_find_neighbor_with_smallest_error():
+    """Test the find_neighbor_with_smallest_error function"""
+    neighbors = jnp.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [8, 9, 10]])
+    clearing_errors = jnp.array([1, 2, 3, 4])
+    neighbor, error = find_neighbor_with_smallest_error(neighbors, clearing_errors)
+
+    assert jnp.allclose(neighbor, jnp.array([1, 2, 3]))
+    assert jnp.allclose(error, jnp.array(1))
+
+
+if __name__ == "__main__":
+    test_generate_random_price_vector()
+    test_filter_out_tabu_neighbors()
