8e9ad9513b5b94eac073d6aff48417964bdf1028
mdesevricourt <m.desevricourt@hotmail.fr>
2024-01-03 17:41:16 -0500

    Update market_level.py
    create vectorized version of aggregate quantities, change name of variable and jit the function that computes aggregate quantities

--- cubiclematch_jax/market_level.py
+++ cubiclematch_jax/market_level.py
@@ -1,6 +1,5 @@
 """Computes market level quantities from prices and primitives."""
 
-from ast import mod
 
 import jax
 import jax.numpy as jnp
@@ -15,19 +14,21 @@ from cubiclematch_jax.demand import (
 from cubiclematch_jax.price import price_bundles
 
 
-def aggregate_quantities(
-    prices: jax.Array,
+@jax.jit
+def compute_aggregate_quantities(
+    price_vector: jax.Array,
     budgets: jax.Array,
     U_tilde: jax.Array,
     bundles: jax.Array,
     supply: jax.Array,
 ):
-    bundle_prices = price_bundles(bundles, prices)
+    bundle_prices = price_bundles(bundles, price_vector)
     demand, excess_budgets = demand_vector(budgets, U_tilde, bundles, bundle_prices)
     agg_demand = compute_aggregate_demand(demand)
     excess_demand_vec = compute_excess_demand(agg_demand, supply)
-    z = modified_excess_demand(excess_demand_vec, prices)
+    z = modified_excess_demand(excess_demand_vec, price_vector)
     alpha = compute_clearing_error(z)
+    number_excess_demands = jnp.sum(excess_demand_vec > 0)
 
     res = {
         "demand": demand,
@@ -36,6 +37,12 @@ def aggregate_quantities(
         "excess_demand_vec": excess_demand_vec,
         "z": z,
         "alpha": alpha,
+        "number_excess_demands": number_excess_demands,
     }
 
     return res
+
+
+compute_aggregate_quantities_vec = jax.vmap(
+    compute_aggregate_quantities, in_axes=(0, None, None, None, None)
+)
