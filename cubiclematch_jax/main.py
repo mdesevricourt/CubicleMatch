719ddea4232233376c8d0d21e258ece19ca7ee1a
mdesevricourt <m.desevricourt@hotmail.fr>
2024-01-07 18:29:53 -0500

    Update main.py

--- cubiclematch_jax/main.py
+++ cubiclematch_jax/main.py
@@ -8,6 +8,7 @@ from cubiclematch_jax.find_ACE import ACE_algorithm, ACE_iteration_extended
 from cubiclematch_jax.individual_demand import calculate_demand_vector
 from cubiclematch_jax.market_level import compute_agg_quantities
 from cubiclematch_jax.neighbors import find_all_neighbors
+from cubiclematch_jax.price import price_bundles
 
 
 class Agent(Protocol):
@@ -44,28 +45,28 @@ def main(
     num_gradient_num = int(settings.pop("num_gradient_num", 10))
     step_sizes = jnp.logspace(-1, 1, num_gradient_num)
     budgets = jnp.array([agent.budget for agent in agents])
-    total_budget = compute_total_budget(budgets)
+    max_budget = float(jnp.max(budgets))
     preference_orderings = jnp.array([agent.preferences for agent in agents])
     length = len(supply)
 
     def compute_demand_vector(price_vec):
+        bundles_prices = price_bundles(price_vec, bundles)
         return calculate_demand_vector(
-            price_vec,
+            bundles_prices,
             budgets,
-            preference_ordering=preference_orderings,
-            available_bundles=bundles,
+            preference_orderings,
+            bundles,
         )
 
-    def evaluate_price_vec(price_vec):
+    def evaluate_prices(price_vec):
         return compute_agg_quantities(
             price_vec,
-            bundles,
             compute_demand_vector,
             supply,
         )
 
     def gen_rand_price_vec(key):
-        return generate_random_price_vector(length, total_budget, key)
+        return generate_random_price_vector(length, max_budget, key)
 
     def find_neighbors(price_vec, agg_dict, tabu_list):
         find_all_neighbors(
@@ -73,14 +74,14 @@ def main(
             excess_demand=agg_dict["excess_demand_vec"],
             excess_budgets=agg_dict["excess_budgets"],
             step_sizes=step_sizes,
-            compute_agg_quantities=evaluate_price_vec,
+            compute_agg_quantities=evaluate_prices,
             tabu_list=tabu_list,
         )
 
     def ACE_iteration(key):
         return ACE_iteration_extended(
             key,
-            evaluate_price_vec,
+            evaluate_prices,
             gen_rand_price_vec,
             find_neighbors,
             max_C=int(max_C),
